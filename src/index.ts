39ddfa6f6e6e678b05e31f5499552bb23e7f8eb0
kris <kris@dorg.tech>
2024-10-02 18:24:34 +0300

    now using redis url directly

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,11 @@ dotenv.config();
 export const app = express();
 app.use(express.json());
 
-export const redis = new Redis({
+export const redis = process.env.NODE_ENV === 'production'
+  ? new Redis(process.env.REDIS_URL ?? '')
+  : new Redis({
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT!),
-    username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
 });
 
